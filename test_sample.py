346500631f5eb0d0e194fe71b3290a0a78cfa98a
DEVichael <156222302+DEVichael@users.noreply.github.com>
2025-11-09 17:12:10 +0100

    Add files via upload

--- test_sample.py
+++ test_sample.py
@@ -1,8 +1,27 @@
 import unittest
+from calculator import Calculator
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+class TestCalculator(unittest.TestCase):
 
-if __name__ == '__main__':
+    def test_sum(self):
+        c = Calculator(10, 5)
+        self.assertEqual(c.sum(), 15)
+
+    def test_subtract(self):
+        c = Calculator(10, 5)
+        self.assertEqual(c.subtract(), 5)
+
+    def test_multiply(self):
+        c = Calculator(4, 3)
+        self.assertEqual(c.multiply(), 12)
+
+    def test_divide(self):
+        c = Calculator(10, 2)
+        self.assertEqual(c.divide(), 5)
+
+    def test_divide_by_zero(self):
+        c = Calculator(10, 0)
+        self.assertEqual(c.divide(), "Błąd: dzielenie przez 0!")
+
+if __name__ == "__main__":
     unittest.main()
